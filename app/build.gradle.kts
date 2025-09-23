d52c686a218f3988ec9a5b3f7b5ff35f0d6032b8
davigo2411 <david.gonzalez.ortega@estudiantat.upc.edu>
2025-09-23 20:51:54 +0200

    fix: actualiza compileSdk y targetSdk para compatibilidad con dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.topseriesapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.topseriesapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
